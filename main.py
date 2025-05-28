72721c3a45ec79ebb76e1951356e892178b3acb5
Rob Hoover <robert.hoover@mac.com>
2025-05-27 17:49:37 -0700

    not finished yet

--- main.py
+++ main.py
@@ -1,20 +1,52 @@
+from stats import get_word_count
+from stats import get_character_count_dict
+
+
+"""
+============ BOOKBOT ============
+Analyzing book found at books/frankenstein.txt...
+----------- Word Count ----------
+Found 75767 total words
+--------- Character Count -------
+e: 44538
+t: 29493
+a: 25894
+o: 24494
+i: 23927
+ô: 1
+============= END ===============
+"""
+
 
 def read_file(file_name):
     with open(file_name) as f:
         file_contents = f.read()
     return file_contents
 
-def count_words(input):
-    return len(input.split())
+
+
+def get_sorted_dictionaries(dict):
+    report_dict = {}
+
+    for key, value in dict:
+        report_dict = { "char": key, "num": value }
+
+    return report_dict
 
 
 def main():
     file_name = "./books/frankenstein.txt"
     book_text = read_file(file_name)
-    num_words = count_words(book_text)
+    num_words = get_word_count(book_text)
+    num_chars_dict = get_character_count_dict(book_text)
+
+    report_dict = {}
+    for key, value in num_chars_dict:
+        report_dict = { "char": key, "num": value }
 
     print(f"{num_words} words found in the document")
-
+#     print(num_chars_dict)
+    print(report_dict)
 
 
 main()
---
+++ stats.py
@@ -0,0 +1,28 @@
+def get_word_count(input):
+    return len(input.split())
+
+def get_character_count_dict(input):
+    letters_dict = {}
+
+    for letter in input:
+        lower_letter = letter.lower()
+        if lower_letter in letters_dict:
+            letters_dict[lower_letter] += 1
+        else:
+            letters_dict[lower_letter] = 1
+
+    return letters_dict
+
+def get_sorted_dictionary(dict):
+    sorted_dict = {}
+    sorted_dict = sorted(dict.items(), key=lambda item: item[1], reverse=True)
+
+    return sorted_dict
+
+def get_sorted_dictionaries(dict):
+    report_dict = {}
+
+    for key, value in dict:
+        report_dict = { "char": key, "num": value }
+
+    return report_dict
