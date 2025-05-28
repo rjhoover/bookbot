4c7e1eb2bfa352a499ec1b7feaba6d44e61b2362
Rob Hoover <robert.hoover@mac.com>
2025-05-28 14:17:03 -0700

    assignment completed

--- main.py
+++ main.py
@@ -1,21 +1,5 @@
+import sys
 from stats import *
-# from stats import get_word_count
-# from stats import get_character_count_dict
-
-"""
-============ BOOKBOT ============
-Analyzing book found at books/frankenstein.txt...
------------ Word Count ----------
-Found 75767 total words
---------- Character Count -------
-e: 44538
-t: 29493
-a: 25894
-o: 24494
-i: 23927
-ô: 1
-============= END ===============
-"""
 
 def print_report(book_file, num_words, list_of_dict):
     print("============ BOOKBOT ============")
@@ -30,15 +14,19 @@ def print_report(book_file, num_words, list_of_dict):
 
     print("============= END ===============")
 
-
-
 def read_file(file_name):
     with open(file_name) as f:
         file_contents = f.read()
     return file_contents
 
 def main():
-    file_name = "./books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    file_name = sys.argv[1] 
+
+    # file_name = "./books/frankenstein.txt"
     book_text = read_file(file_name)
     num_words = get_word_count(book_text)
     num_chars_dict = get_character_count_dict(book_text)
@@ -46,8 +34,7 @@ def main():
     sorted_list = get_sorted_list_of_dict(unsorted_list_of_dict)
 
     print_report(file_name, num_words, sorted_list)
-    # print(f"{num_words} words found in the document")
-    # print(sorted_list)
-
 
 main()
+
+
